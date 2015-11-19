a86bd96b571f0caf4b49e47a1f35ce4fb52ab529
kmramanathan <ram.dvk@gmail.com>
2015-11-19 16:18:24 +0530

    Alias search changes continues to previous commit

---
+++ sql/alias_search.sql
@@ -0,0 +1,2 @@
+insert into bgc_products (bgc_product_id, bgc_product_name, price_basic, price_dpass, bgc_product_desc, nss_product_id)
+	values (7, 'USAlias', 14.95, 14.95, 'US Alias Search Nationwide', 7);
